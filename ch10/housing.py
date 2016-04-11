c6ee951ea09f254d98ba737f7e0e25fb1e197f42
noprom <tyee.noprom@qq.com>
2016-04-11 14:15:55 +0800

    add data

--- ch10/housing.py
+++ ch10/housing.py
@@ -6,6 +6,6 @@
 Created on 4/11/16
 '''
 import pandas as pd
-df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/housing/housing.data', header = None, sep = '\s+')
+df = pd.read_csv('./housing.data', header = None, sep = '\s+')
 df.columns = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV']
 df.head()
