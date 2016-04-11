22c0d121b140b689cdf4f5ba56581296001a4d62
noprom <tyee.noprom@qq.com>
2016-04-11 15:31:53 +0800

    corelation the data

--- ch10/housing.py
+++ ch10/housing.py
@@ -6,10 +6,12 @@
 @Time: 4/11/16
 '''
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 df = pd.read_csv('./housing.data', header=None, sep='\s+')
+cols = ['LSTAT', 'INDUS', 'NOX', 'RM', 'MEDV']
 
 
 def look_data():
@@ -28,9 +30,19 @@ def visualize_data():
     :return:
     '''
     sns.set(style='whitegrid', context='notebook')
-    cols = ['LSTAT', 'INDUS', 'NOX', 'RM', 'MEDV']
     sns.pairplot(df[cols], size=2.5)
     plt.show()
 
+
+def corelation_data():
+    '''
+    发现数据之间的关联关系
+    :return:
+    '''
+    cm = np.corrcoef(df[cols].values.T)
+    sns.set(font_scale=1.5)
+    hm = sns.heatmap(cm, cbar=True, annot=True, fmt='.2f', annot_kws={'size': 15}, yticklabels=cols, xticklabels=cols)
+
 look_data()
 visualize_data()
+corelation_data()
