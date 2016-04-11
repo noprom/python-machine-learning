9d09258d76ee8402b49185d6646cd743448bcae5
noprom <tyee.noprom@qq.com>
2016-04-11 12:14:20 +0800

    add housing

---
+++ ch10/housing.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+'''
+@title:Predicting Continuous Target Variables with Regression Analysis
+
+@author: tyee.noprom@qq.com
+Created on 4/11/16
+'''
+import pandas as pd
+df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/housing/housing.data', header = None, sep = '\s+')
+df.columns = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV']
+df.head()
