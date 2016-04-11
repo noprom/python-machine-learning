1d968b8e4ddaa488d1961c06ec31ad2bee1fcb99
noprom <tyee.noprom@qq.com>
2016-04-11 15:01:08 +0800

    visualize the data

--- ch10/housing.py
+++ ch10/housing.py
@@ -5,15 +5,32 @@
 @Author: tyee.noprom@qq.com
 @Time: 4/11/16
 '''
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv('./housing.data', header=None, sep='\s+')
+
 
 def look_data():
     '''
     查看数据
     :return:
     '''
-    import pandas as pd
-    df = pd.read_csv('./housing.data', header = None, sep = '\s+')
-    df.columns = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV']
+    df.columns = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT',
+                  'MEDV']
     print(df.head())
 
+
+def visualize_data():
+    '''
+    可视化数据
+    :return:
+    '''
+    sns.set(style='whitegrid', context='notebook')
+    cols = ['LSTAT', 'INDUS', 'NOX', 'RM', 'MEDV']
+    sns.pairplot(df[cols], size=2.5)
+    plt.show()
+
 look_data()
+visualize_data()
