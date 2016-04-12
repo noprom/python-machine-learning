15c3fab323aae2aa422683cfee1e40f3efc2dfb8
noprom <tyee.noprom@qq.com>
2016-04-12 15:23:46 +0800

    add perception

--- ch02/perception.py
+++ ch02/perception.py
@@ -5,3 +5,25 @@
 @Author: tyee.noprom@qq.com
 @Time: 4/12/16 3:13 PM
 '''
+import numpy as np
+
+
+class Perception(object):
+    """Perception classifier
+        Parameters
+    ------------
+    eta : float
+        Learning rate (between 0.0 and 1.0)
+    n_iter : int
+        Passes over the training dataset.
+    Attributes
+    -----------
+    w_ : 1d-array
+        Weights after fitting.
+    errors_ : list
+        Number of misclassifications in every epoch.
+    """
+
+    def __init__(self, eta=0.01, n_iter=10):
+        self.eta = eta
+        self.n_iter = n_iter
