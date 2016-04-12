70cf59bb2a0e13609f681eb59360e68deb3ee9d3
noprom <tyee.noprom@qq.com>
2016-04-12 17:21:56 +0800

    show the data

--- ch02/perception.py
+++ ch02/perception.py
@@ -7,9 +7,19 @@
 '''
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 df = pd.read_csv('./iris.data', header=None)
 print(df.tail())
+y = df.iloc[0:100, 4].values  # 取出第四列分类数据
+y = np.where(y == 'Iris-setosa', -1, 1)
+X = df.iloc[0:100, [0, 2]].values
+plt.scatter(X[:50, 0], X[:50, 1], color='red', marker='o', label='setosa')
+plt.scatter(X[50:100, 0], X[50:100, 1], color='blue', marker='x', label='versicolor')
+plt.xlabel('petal length')
+plt.ylabel('sepal length')
+plt.legend(loc='upper left')
+plt.show()
 
 
 class Perception(object):
