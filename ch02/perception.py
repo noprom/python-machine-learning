87451bd564e6f63929ee1d5fc6c0aee4b9928722
noprom <tyee.noprom@qq.com>
2016-04-12 16:31:59 +0800

    add fit

--- ch02/perception.py
+++ ch02/perception.py
@@ -40,3 +40,15 @@ class Perception(object):
         -------
         self : object
         """
+        self.w_ = np.zeros(1 + X.shape[1])
+        self.errors_ = []
+
+        for _ in range(self.n_iter):
+            errors = 0
+            for xi, target in zip(X, y):
+                update = self.eta * (target - self.predict(xi))
+                self.w_[1:] += update * xi
+                self.w_[0] += update
+                errors += int(update != 1.0)
+            self.errors_.append(errors)
+        return self
