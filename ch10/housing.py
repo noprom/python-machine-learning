36768118c7c1fe87145853b3f95f20641075cabe
noprom <tyee.noprom@qq.com>
2016-05-19 23:48:33 +0800

    housing_polynomial_regression

--- ch10/housing.py
+++ ch10/housing.py
@@ -10,6 +10,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
 from sklearn.metrics import r2_score
 from sklearn.cross_validation import train_test_split
 from sklearn.metrics import mean_squared_error
@@ -203,7 +204,6 @@ def polynomial_regression():
     多次问题
     :return:
     '''
-    from sklearn.preprocessing import PolynomialFeatures
     X = np.array([258.0, 270.0, 294.0,
                   320.0, 342.0, 368.0,
                   396.0, 446.0, 480.0,
@@ -243,10 +243,47 @@ def polynomial_regression():
         r2_score(y, y_lin_pred), r2_score(y, y_quad_pred)))
 
 
+def housing_polynomial_regression():
+    '''
+    housing数据1元多次
+    :return:
+    '''
+    X = df[['LSTAT']].values
+    y = df['MEDV'].values
+    regr = LinearRegression()
+    # create polynomial features
+    quadratic = PolynomialFeatures(degree=2)
+    cubic = PolynomialFeatures(degree=3)
+    X_quad = quadratic.fit_transform(X)
+    X_cubic = cubic.fit_transform(X)
+    # linear fit
+    X_fit = np.arange(X.min(), X.max(), 1)[:, np.newaxis]
+    regr = regr.fit(X, y)
+    y_lin_fit = regr.predict(X_fit)
+    linear_r2 = r2_score(y, regr.predict(X))
+    # quadratic fit
+    regr = regr.fit(X_quad, y)
+    y_quad_fit = regr.predict(quadratic.fit_transform(X_fit))
+    quadratic_r2 = r2_score(y, regr.predict(X_quad))
+    # cubic fit
+    regr = regr.fit(X_cubic, y)
+    y_cubic_fit = regr.predict(cubic.fit_transform(X_fit))
+    cubic_r2 = r2_score(y, regr.predict(X_cubic))
+    # plot results
+    plt.scatter(X, y, label='training points', color='lightgray')
+    plt.plot(X_fit, y_lin_fit, label='linear (d=1), $R^2=%.2f$' % linear_r2, color='blue', lw=2, linestyle=':')
+    plt.plot(X_fit, y_quad_fit, label='quadratic (d=2), $R^2=%.2f$' % quadratic_r2, color='red', lw=2, linestyle='-')
+    plt.plot(X_fit, y_cubic_fit, label='cubic (d=3), $R^2=%.2f$' % cubic_r2, color='green', lw=2, linestyle='--')
+    plt.xlabel('% lower status of the population [LSTAT]')
+    plt.ylabel('Price in $1000\'s [MEDV]')
+    plt.legend(loc='upper right')
+    plt.show()
+
 # look_data()
 # visualize_data()
 # corelation_data()
 # linear_model()
 # ransac_fit(X, y)
 # multi_regression()
-polynomial_regression()
+# polynomial_regression()
+housing_polynomial_regression()
