39d3b90ad6792c92482443877e7e8d19a61c5d3f
noprom <tyee.noprom@qq.com>
2016-05-19 12:29:04 +0800

    ransac_fit

--- ch10/housing.py
+++ ch10/housing.py
@@ -9,6 +9,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.linear_model import LinearRegression
+from sklearn.cross_validation import train_test_split
 
 
 class LinearRegressionGD(object):
@@ -43,6 +45,10 @@ class LinearRegressionGD(object):
 
 df = pd.read_csv('./housing.data', header=None, sep='\s+')
 cols = ['LSTAT', 'INDUS', 'NOX', 'RM', 'MEDV']
+df.columns = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT',
+              'MEDV']
+X = df[['RM']].values
+y = df['MEDV'].values
 
 
 def look_data():
@@ -80,8 +86,6 @@ def linear_model():
     线性回归
     :return:
     '''
-    X = df[['RM']].values
-    y = df['MEDV'].values
     from sklearn.preprocessing import StandardScaler
     sc_x = StandardScaler()
     sc_y = StandardScaler()
@@ -113,7 +117,37 @@ def lin_regplot(X, y, model):
     return None
 
 
-look_data()
+def ransac_fit(X, y):
+    '''
+    一个强健的fit
+    :return:
+    '''
+    from sklearn.linear_model import RANSACRegressor
+    ransac = RANSACRegressor(LinearRegression(),
+                             max_trials=100,
+                             min_samples=50,
+                             residual_metric=lambda x: np.sum(np.abs(x), axis=1),
+                             residual_threshold=5.0,
+                             random_state=0)
+    ransac.fit(X, y)
+    # plot
+    inlier_mask = ransac.inlier_mask_
+    outlier_mask = np.logical_not(inlier_mask)
+    line_X = np.arange(3, 10, 1)
+    line_y_ransac = ransac.predict(line_X[:, np.newaxis])
+    plt.scatter(X[inlier_mask], y[inlier_mask],
+                c='blue', marker='o', label='Inliers')
+    plt.scatter(X[outlier_mask], y[outlier_mask],
+                c='lightgreen', marker='s', label='Outliers')
+    plt.plot(line_X, line_y_ransac, color='red')
+    plt.xlabel('Average number of rooms [RM]')
+    plt.ylabel('Price in $1000\'s [MEDV]')
+    plt.legend(loc='upper left')
+    plt.show()
+
+
+# look_data()
 # visualize_data()
 # corelation_data()
-linear_model()
+# linear_model()
+ransac_fit(X, y)
