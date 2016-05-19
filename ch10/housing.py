e07a312c98f90ee39fb41bc916cfc66977f17927
noprom <tyee.noprom@qq.com>
2016-05-19 22:20:39 +0800

    polynomial_regression

--- ch10/housing.py
+++ ch10/housing.py
@@ -10,7 +10,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
 from sklearn.cross_validation import train_test_split
+from sklearn.metrics import mean_squared_error
 
 
 class LinearRegressionGD(object):
@@ -163,7 +165,6 @@ def multi_regression():
     y_train_pred = slr.predict(X_train)
     y_test_pred = slr.predict(X_test)
     # 计算Mean Squared Error (MSE)
-    from sklearn.metrics import mean_squared_error
     print('MSE train: %.3f, test: %.3f' % (
         mean_squared_error(y_train, y_train_pred),
         mean_squared_error(y_test, y_test_pred)))
@@ -171,9 +172,7 @@ def multi_regression():
 
     # 计算R*R
     # If R*R =1, the model  ts the data perfectly with a corresponding MSE = 0 .
-    from sklearn.metrics import r2_score
     print('R^2 train: %.3f, test: %.3f' % (r2_score(y_train, y_train_pred), r2_score(y_test, y_test_pred)))
-
 
     # plot
     plt.scatter(y_train_pred, y_train_pred - y_train, c='blue', marker='o', label='Training data')
@@ -186,9 +185,68 @@ def multi_regression():
     plt.show()
 
 
+def ridge_regression():
+    '''
+    regularized regression
+    :return:
+    '''
+    from sklearn.linear_model import Ridge
+    ridge = Ridge(alpha=1.0)
+    from sklearn.linear_model import Lasso
+    lasso = Lasso(alpha=1.0)
+    from sklearn.linear_model import ElasticNet
+    lasso = ElasticNet(alpha=1.0, l1_ratio=0.5)
+
+
+def polynomial_regression():
+    '''
+    多次问题
+    :return:
+    '''
+    from sklearn.preprocessing import PolynomialFeatures
+    X = np.array([258.0, 270.0, 294.0,
+                  320.0, 342.0, 368.0,
+                  396.0, 446.0, 480.0,
+                  586.0])[:, np.newaxis]
+    y = np.array([236.4, 234.4, 252.8,
+                  298.6, 314.2, 342.2,
+                  360.8, 368.0, 391.2,
+                  390.8])
+    # 1. Add a second degree polynomial term
+    lr = LinearRegression()
+    pr = LinearRegression()
+    # 2次方
+    quadratic = PolynomialFeatures(degree=2)
+    X_quad = quadratic.fit_transform(X)
+
+    # 2.Fit a simple linear regression model for comparison:
+    lr.fit(X, y)
+    X_fit = np.arange(250, 600, 10)[:, np.newaxis]
+    y_lin_fit = lr.predict(X_fit)
+    # 3. Fit a multiple regression model on the transformed features for polynomial regression:
+    pr.fit(X_quad, y)
+    y_quad_fit = pr.predict(quadratic.fit_transform(X_fit))
+    # 4.Plot the results:
+    plt.scatter(X, y, label='training points')
+    plt.plot(X_fit, y_lin_fit, label='linear fit', linestyle='--')
+    plt.plot(X_fit, y_quad_fit, label='quadratic fit')
+    plt.legend(loc='upper left')
+    plt.show()
+    # 5.Predict the outcome
+    y_lin_pred = lr.predict(X)
+    y_quad_pred = pr.predict(X_quad)
+    # Training MSE linear: 569.780, quadratic: 61.330
+    print('Training MSE linear: %.3f, quadratic: %.3f' % (
+        mean_squared_error(y, y_lin_pred), mean_squared_error(y, y_quad_pred)))
+    # Training  R^2 linear: 0.832, quadratic: 0.982
+    print('Training  R^2 linear: %.3f, quadratic: %.3f' % (
+        r2_score(y, y_lin_pred), r2_score(y, y_quad_pred)))
+
+
 # look_data()
 # visualize_data()
 # corelation_data()
 # linear_model()
 # ransac_fit(X, y)
-multi_regression()
+# multi_regression()
+polynomial_regression()
