009c75d3be0dfabeaacf3c1341f0c084d43d1f1a
noprom <tyee.noprom@qq.com>
2016-05-19 10:44:30 +0800

    linear model

--- ch10/housing.py
+++ ch10/housing.py
@@ -10,6 +10,37 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+
+class LinearRegressionGD(object):
+    '''
+    线性回归模型,梯度下降
+    使用最小二乘法
+    '''
+
+    def __init__(self, eta=0.001, n_iter=20):
+        self.eta = eta
+        self.n_iter = n_iter
+
+    def fit(self, X, y):
+        self.w_ = np.zeros(1 + X.shape[1])
+        self.cost_ = []
+
+        for i in range(self.n_iter):
+            output = self.net_input(X)
+            errors = (y - output)
+            self.w_[1:] += self.eta * X.T.dot(errors)
+            self.w_[0] += self.eta * errors.sum()
+            cost = (errors ** 2).sum() / 2.0
+            self.cost_.append(cost)
+        return self
+
+    def net_input(self, X):
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+
+    def predict(self, X):
+        return self.net_input(X)
+
+
 df = pd.read_csv('./housing.data', header=None, sep='\s+')
 cols = ['LSTAT', 'INDUS', 'NOX', 'RM', 'MEDV']
 
@@ -39,40 +70,32 @@ def corelation_data():
     '''
     cm = np.corrcoef(df[cols].values.T)
     sns.set(font_scale=1.5)
+    # 热力图
     hm = sns.heatmap(cm, cbar=True, annot=True, fmt='.2f', annot_kws={'size': 15}, yticklabels=cols, xticklabels=cols)
     plt.show()
 
 
-look_data()
-visualize_data()
-corelation_data()
-
-
-class LinearRegressionGD(object):
+def linear_model():
     '''
-    线性回归模型
-    使用最小二乘法
+    线性回归
+    :return:
     '''
+    X = df[['RM']].values
+    y = df['MEDV'].values
+    from sklearn.preprocessing import StandardScaler
+    sc_x = StandardScaler()
+    sc_y = StandardScaler()
+    X_std = sc_x.fit_transform(X)
+    y_std = sc_y.fit_transform(y)
+    lr = LinearRegressionGD()
+    lr.fit(X_std, y_std)
+    plt.plot(range(1, lr.n_iter + 1), lr.cost_)
+    plt.ylabel('SSE')
+    plt.xlabel('Epoch')
+    plt.show()
 
-    def __init__(self, eta=0.001, n_iter=20):
-        self.eta = eta
-        self.n_iter = n_iter
 
-    def fit(self, X, y):
-        self.w_ = np.zeros(1 + X.shape[1])
-        self.cost_ = []
-
-        for i in range(self.n_iter):
-            output = self.net_input(X)
-            errors = (y - output)
-            self.w_[1:] += self.eta * X.T.dot(errors)
-            self.w_[0] += self.eta * errors.sum()
-            cost = (errors ** 2).sum() / 2.0
-            self.cost_.append(cost)
-        return self
-
-    def net_input(self, X):
-        return np.dot(X, self.w_[1:]) + self.w_[0]
-
-    def predict(self, X):
-        return self.net_input(X)
+# look_data()
+# visualize_data()
+# corelation_data()
+linear_model()
