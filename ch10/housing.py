fdeaffda8f5a43569b0566cdb36cfce6601d48da
noprom <tyee.noprom@qq.com>
2016-05-20 19:13:13 +0800

    add random_forest_tree_regression

--- ch10/housing.py
+++ ch10/housing.py
@@ -322,6 +322,30 @@ def decision_tree_regression():
     plt.show()
 
 
+def random_forest_tree_regression():
+    X = df.iloc[:, :-1].values
+    y = df['MEDV'].values
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=1)
+    from sklearn.ensemble import RandomForestRegressor
+    forest = RandomForestRegressor(n_estimators=1000, criterion='mse', random_state=1, n_jobs=-1)
+    forest.fit(X_train, y_train)
+    y_train_pred = forest.predict(X_train)
+    y_test_pred = forest.predict(X_test)
+    print('MSE train: %.3f, test: %.3f' % (
+        mean_squared_error(y_train, y_train_pred), mean_squared_error(y_test, y_test_pred)))
+    print('R^2 train: %.3f, test: %.3f' % (
+        r2_score(y_train, y_train_pred), r2_score(y_test, y_test_pred)))
+    # MSE train: 3.235, test: 11.635
+    # R^2 train: 0.960, test: 0.871
+    plt.scatter(y_train_pred, y_train_pred - y_train, c='black', marker='o', s=35, alpha=0.5, label='Training data')
+    plt.scatter(y_test_pred, y_test_pred - y_test, c='lightgreen', marker='s', s=35, alpha=0.7, label='Test data')
+    plt.xlabel('Predicted values')
+    plt.ylabel('Residuals')
+    plt.legend(loc='upper left')
+    plt.hlines(y=0, xmin=-10, xmax=50, lw=2, color='red')
+    plt.xlim([-10, 50])
+    plt.show()
+
 # look_data()
 # visualize_data()
 # corelation_data()
@@ -331,4 +355,5 @@ def decision_tree_regression():
 # polynomial_regression()
 # housing_polynomial_regression()
 # log_feature()
-decision_tree_regression()
+# decision_tree_regression()
+random_forest_tree_regression()
