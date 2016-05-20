d823c440c05b7f14bed0d0c5636e7228ecbf7a80
noprom <tyee.noprom@qq.com>
2016-05-20 18:11:51 +0800

    add log_feature

--- ch10/housing.py
+++ ch10/housing.py
@@ -279,6 +279,32 @@ def housing_polynomial_regression():
     plt.legend(loc='upper right')
     plt.show()
 
+
+def log_feature():
+    '''
+     a log transformation of the LSTAT feature variable and the square root of MEDV
+    :return:
+    '''
+    # transform features
+    X = df[['LSTAT']].values
+    y = df['MEDV'].values
+    X_log = np.log(X)
+    y_sqrt = np.sqrt(y)
+    regr = LinearRegression()
+    # fit features
+    X_fit = np.arange(X_log.min() - 1, X_log.max() + 1, 1)[:, np.newaxis]
+    regr = regr.fit(X_log, y_sqrt)
+    y_lin_fit = regr.predict(X_fit)
+    linear_r2 = r2_score(y_sqrt, regr.predict(X_log))
+    # plot results
+    plt.scatter(X_log, y_sqrt, label='training points', color='lightgray')
+    plt.plot(X_fit, y_lin_fit, label='linear (d=1), $R^2=%.2f$' % linear_r2, color='blue', lw=2)
+    plt.xlabel('log(% lower status of the population [LSTAT])')
+    plt.ylabel('$\sqrt{Price \; in \; \$1000\'s [MEDV]}$')
+    plt.legend(loc='lower left')
+    plt.show()
+
+
 # look_data()
 # visualize_data()
 # corelation_data()
@@ -286,4 +312,5 @@ def housing_polynomial_regression():
 # ransac_fit(X, y)
 # multi_regression()
 # polynomial_regression()
-housing_polynomial_regression()
+# housing_polynomial_regression()
+# log_feature()
