365b45b7d5c96dbb52a54b98188c4e8419abb115
noprom <tyee.noprom@qq.com>
2016-05-21 17:45:14 +0800

    multable exchange

--- ch12/image-identification.py
+++ ch12/image-identification.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 '''
-@Title:image-describer
+@Title:image-identification
 
 @Author: tyee.noprom@qq.com
 @Time: 5/21/16 4:16 PM
@@ -25,6 +25,28 @@ def load_mnist(path, kind='train'):
     return images, labels
 
 
+def save_bin2csv(X_train, y_train, X_test, y_test):
+    '''
+    将读取到的二进制转化为csv文件
+    :return:
+    '''
+    np.savetxt('train_img.csv', X_train, fmt='%i', delimiter=',')
+    np.savetxt('train_labels.csv', y_train, fmt='%i', delimiter=',')
+    np.savetxt('test_img.csv', X_test, fmt='%i', delimiter=',')
+    np.savetxt('test_labels.csv', y_test, fmt='%i', delimiter=',')
+
+
+def load_from_csv():
+    '''
+    从csv文件读取
+    :return:
+    '''
+    X_train = np.genfromtxt('train_img.csv', dtype=int, delimiter=',')
+    y_train = np.genfromtxt('train_labels.csv', dtype=int, delimiter=',')
+    X_test = np.genfromtxt('test_img.csv', dtype=int, delimiter=',')
+    y_test = np.genfromtxt('test_labels.csv', dtype=int, delimiter=',')
+
+
 mnist_path = '../data/mnist'
 X_train, y_train = load_mnist(mnist_path, kind='train')
 print('Rows: %d, columns: %d' % (X_train.shape[0], X_train.shape[1]))
@@ -38,6 +60,18 @@ fig, ax = plt.subplots(nrows=2, ncols=5, sharex=True, sharey=True, )
 ax = ax.flatten()
 for i in range(10):
     img = X_train[y_train == i][0].reshape(28, 28)
+    ax[i].imshow(img, cmap='Greys', interpolation='nearest')
+ax[0].set_xticks([])
+ax[0].set_yticks([])
+plt.tight_layout()
+plt.show()
+
+# In addition, let's also plot multiple examples of the same digit to see how different those handwriting examples really are:
+
+fig, ax = plt.subplots(nrows=5, ncols=5, sharex=True, sharey=True, )
+ax = ax.flatten()
+for i in range(25):
+    img = X_train[y_train == 7][i].reshape(28, 28)
     ax[i].imshow(img, cmap='Greys', interpolation='nearest')
 ax[0].set_xticks([])
 ax[0].set_yticks([])
