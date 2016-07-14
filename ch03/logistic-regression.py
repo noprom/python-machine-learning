1a140a7e89ea94fc78fe532d2d548479c3e4fbd0
noprom <tyee.noprom@qq.com>
2016-07-14 22:20:00 +0800

    plot_sigmoid

--- ch03/logistic-regression.py
+++ ch03/logistic-regression.py
@@ -23,3 +23,19 @@ def plot_sigmoid():
     绘制sigmoid函数图像
     :return:无
     '''
+    # 创建一个从[-7, 7), 步长为0.1的数组
+    z = np.arange(-7, 7, 0.1)
+    phi_z = sigmoid(z)
+    # 绘图
+    plt.plot(z, phi_z)
+    plt.axvline(0.0, color='k')
+    plt.axhspan(0.0, 1.0, facecolor='1.0', alpha=1.0, ls='dotted')
+    plt.axhline(y=0.5, ls='dotted', color='k')
+    plt.yticks([0.0, 0.5, 1.0])
+    plt.ylim(-0.1, 1.1)
+    plt.xlabel('z')
+    plt.ylabel('$\phi (z)$')
+    plt.show()
+
+
+plot_sigmoid()
