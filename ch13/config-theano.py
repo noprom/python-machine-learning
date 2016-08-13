26487d8e66fddd981c0f379c7dc80627265cf377
noprom <tyee.noprom@qq.com>
2016-08-13 09:30:21 +0800

    ch13/config-theano.py

--- ch13/config-theano.py
+++ ch13/config-theano.py
@@ -7,10 +7,39 @@
 '''
 
 import theano
+import numpy as np
 from theano import scalar as T
 
-print(theano.config.floatX)
 
-theano.config.floatX = 'float32'
+def config():
+    '''
+    config theano
+    :return:
+    '''
+    print(theano.config.floatX)
+    theano.config.floatX = 'float32'
+    print(theano.config.device)
 
-print(theano.config.device)
+
+def arr():
+    '''
+    working with array in
+    :return:
+    '''
+    # initialize
+    x = T.fmatrix(name='x')
+    x_sum = T.sum(x, axis=0)
+
+    # compile
+    calc_sum = theano.function(inputs=[x], outputs=x_sum)
+
+    # execute (Python list)
+    ary = [[1, 2, 3], [1, 2, 3]]
+    print('Column sum:', calc_sum(ary))
+
+    # execute (NumPy array)
+    ary = np.array([[1, 2, 3], [1, 2, 3]], dtype=theano.config.floatX)
+    print('Column sum:', calc_sum(ary))
+
+
+arr()
